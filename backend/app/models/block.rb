6a9f3c2e0d6fbb1280b3c040b11be7d36538f7fa
amy-chinlai <amy@actionnetwork.org>
2022-03-15 10:42:04 -0400

    rails validations

--- backend/app/models/block.rb
+++ backend/app/models/block.rb
@@ -1,4 +1,10 @@
 class Block < ApplicationRecord
     # has_many :if_blocks
     # has_many :buttons, through: :if_blocks
+
+    validates :name, presence: true
+    validates :donation_type, presence: true
+    validates :default, presence: true
+    validates :multiplier, presence: true
+
 end
